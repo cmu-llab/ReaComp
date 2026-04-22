7c2ecb1cca59870abda69ba497126179a223c378
Atharva Naik <atharvanaik2018@gmail.com>
2026-04-22 15:37:01 -0400

    failure analysis script and result

--- scripts/find_hard_unsolved_cases.py
+++ scripts/find_hard_unsolved_cases.py
@@ -19,5 +19,14 @@ if __name__ == "__main__":
     for rec in outputs:
         if rec['task_index'] in unsolved_indices:
             unsolved_cases.append(rec)
+            # remove legacy keys that have no meaning in the context of failure analysis for direct_feedback, etc.
+            del rec['task_spec']
+            del rec['steps_taken']
+            del rec['task_type']
+            del rec["library_snapshot"]
+            # remove agent messages to save space, since the full CoTs are too much.
+            del rec['agent_messages']
+            print(rec.keys())
+
     print(len(unsolved_cases))
     write_jsonl(unsolved_cases, "failure_analysis/pbebench_lite_direct_feedback.jsonl")
